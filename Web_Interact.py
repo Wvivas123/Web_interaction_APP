4b7d3c7c00f360df613eaf10cd06bccbacc1b91e
William Vivas <williamvivasdev@gmail.com>
2020-05-25 15:07:18 -0400

    Added User Input

--- Web_Interact.py
+++ Web_Interact.py
@@ -1,14 +1,17 @@
 from selenium import webdriver
 from time import sleep
 
-pw = '###########______PW_______HERE############'
+username = input('[+] Enter your Username [+]')
+pw = input('[+] Enter your Password [+]')
+
 
 class InstaBot:
     def __init__(self, username, pw):
         self.driver = webdriver.Chrome()
         self.driver.get("https://www.instagram.com/")
         sleep(2)
-        self.driver.find_element_by_xpath('/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[2]/div/label/input')\
+        self.driver.find_element_by_xpath(
+            '/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[2]/div/label/input') \
             .send_keys(username)
         self.driver.find_element_by_xpath(
             '/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[3]/div/label/input') \
@@ -16,14 +19,8 @@ class InstaBot:
         self.driver.find_element_by_xpath(
             '/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[4]/button') \
             .click()
-        sleep(400)
+        sleep(10)
+        self.driver.close()
 
 
-        #self.driver.close()
-
-
-
-
-
-
-InstaBot('############', pw)
+InstaBot(username, pw)
