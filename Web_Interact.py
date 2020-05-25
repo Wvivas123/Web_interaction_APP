c6cf0c7da7a9826cc55a8cd3352893876ed4a96d
William Vivas <williamvivasdev@gmail.com>
2020-05-25 14:38:35 -0400

    finished Login

--- Web_Interact.py
+++ Web_Interact.py
@@ -1,13 +1,29 @@
 from selenium import webdriver
 from time import sleep
 
+pw = 'Mydog#123'
 
 class InstaBot:
-    def __init__(self):
+    def __init__(self, username, pw):
         self.driver = webdriver.Chrome()
         self.driver.get("https://www.instagram.com/")
+        sleep(2)
+        self.driver.find_element_by_xpath('/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[2]/div/label/input')\
+            .send_keys(username)
+        self.driver.find_element_by_xpath(
+            '/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[3]/div/label/input') \
+            .send_keys(pw)
+        self.driver.find_element_by_xpath(
+            '/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[4]/button') \
+            .click()
         sleep(400)
 
 
+        #self.driver.close()
 
-InstaBot()
+
+
+
+
+
+InstaBot('Open.Sourced', pw)
