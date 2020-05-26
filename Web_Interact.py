a5189fb05a14d6b4ae8163e989e7a7beefbe3927
William Vivas <williamvivasdev@gmail.com>
2020-05-26 17:06:17 -0400

    got the app to nav and like

--- Web_Interact.py
+++ Web_Interact.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
 from time import sleep
+from getpass import getpass
+
 
 username = input('[+] Enter your Username [+]')
-pw = input('[+] Enter your Password [+]')
+pw = getpass('[+] Enter your Password [+]')
 
 
 class InstaBot:
@@ -19,8 +21,20 @@ class InstaBot:
         self.driver.find_element_by_xpath(
             '/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[4]/button') \
             .click()
+        sleep(7)
+        self.driver.find_element_by_xpath(
+            '/html/body/div[4]/div/div/div[3]/button[1]') \
+            .click()
+        sleep(5)
+
+        self.driver.get("https://www.instagram.com/explore/tags/openframeworks/")
+        sleep(5)
+        self.driver.find_element_by_xpath("/html/body/div[1]/section/main/article/div[1]/div/div/div[1]/div[1]/a/div/div[2]").click()
         sleep(10)
-        self.driver.close()
+        self.driver.find_element_by_xpath('/html/body/div[4]/div[2]/div/article/div[2]/section[1]/span[1]/button').click()
+        sleep(40)
+
+
 
 
 InstaBot(username, pw)
